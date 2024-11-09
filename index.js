900dc3c254389b04e9a0e5a254480cb2d30d48df
Owethu927 <owethusilela@gmail.com>
2024-11-09 10:39:47 -0600

    added conditional statements in challenge 1

--- index.js
+++ index.js
@@ -5,8 +5,11 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free';  // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
-
+if (userPreference === undefined) {
+    console.log("Regular")
+} else {
+    console.log(`Sugar-free`);
+}
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
